f063de73f3aadac44dedf4d269dfdd9d1b3dcc68
fannyyy1 <fannyekadestiany@gmail.com>
2025-06-28 22:09:53 +0700

    Latihan1b.php

---
+++ Latihan1b.php
@@ -0,0 +1,15 @@
+<?php
+$x = 10;
+
+$perkalian = $x * 8;
+$pembagian = $perkalian / 4;
+$pengurangan = $pembagian - 6;
+$penjumlahan = $pengurangan + 2;
+
+// Tampilkan hasil
+echo "Aku adalah angka $x<br>";
+echo "Jika aku dikali 8, jumlahku sekarang $perkalian<br>";
+echo "Jika aku dibagi 4, jumlahku sekarang $pembagian<br>";
+echo "Jika aku dikurang 6, jumlahku sekarang $pengurangan<br>";
+echo "Jika aku ditambah 2, maka hasil akhirku $penjumlahan<br>";
+?>
