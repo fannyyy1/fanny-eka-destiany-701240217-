6ccc3d6e9c76b49d77c0825e1d5c672cf2711c86
fannyyy1 <fannyekadestiany@gmail.com>
2025-06-28 22:08:47 +0700

    Latihan1a.php

---
+++ Latihan1a.php
@@ -0,0 +1,17 @@
+<?php
+$ilyas ="topi";
+$javava =" bundar";
+echo "$ilyas saya $javava, $javava $ilyas saya"
+?>
+
+
+
+<?php
+function luaspersegi($sisi) {
+    $luas = $sisi = $sisi;
+    return $luas;
+}
+$sisi = 7;
+$luas = luaspersegi($sisi);
+echo "(Luas Persegi dengan sisi " .$sisi . "adalah " . $luas;
+?>
